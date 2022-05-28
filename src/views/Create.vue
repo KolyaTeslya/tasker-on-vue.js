2e113aa6bb48ec09cdba1fc535e93c14443be406
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-05-28 22:16:13 +0300

    Single page application on Vue.js for creating tasks

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,7 +3,7 @@
     <div class="col s6 offset-s3">
       <h1>Create task</h1>
 
-      <form>
+      <form @submit.prevent="submitHandler">
         <div class="input-field">
           <input id="title" v-model="title" type="text" class="validate" required>
           <label for="title">Title</label>
@@ -14,9 +14,13 @@
 
         <div class="input-field">
           <textarea v-model="description" id="discription" class="materialize-textarea"></textarea>
-          <label for="discription">Textarea</label>
+          <label for="discription">Description</label>
           <span class="character-counter" style="float: right; font-size: 12px;">{{description.length}}/2048</span>
         </div>
+
+        <input type="text" ref="datepicker">
+
+        <button class="btn" type="submit">Create task</button>
       </form>
     </div>
   </div>
@@ -27,12 +31,42 @@ export default {
   name: 'create',
   data: () => ({
     description: '',
-    title: ''
+    title: '',
+    chips: null,
+    date: null,
   }),
   mounted() {
-    M.Chips.init(this.$refs.chips, {
+    this.chips = M.Chips.init(this.$refs.chips, {
       placeholder: 'Task tags'
+    })
+    this.date = M.Datepicker.init(this.$refs.datepicker, {
+      format: 'dd.mm.yyyy',
+      defaultDate: new Date(),
+      setDefaultDate: true
     });
+  },
+  methods:{
+    submitHandler(){
+       const task ={
+         title: this.title,
+         description: this.description,
+         id: Date.now(),
+         status: 'active',
+         tags: this.chips.chipsData,
+         date: this.date.date
+       }
+
+       this.$store.dispatch('createTask', task)
+      this.$router.push('/list')
+    }
+  },
+  destroyed() {
+    if (this.date && this.date.destroy){
+      this.date.destroy()
+    }
+    if (this.chips && this.chips.destroy){
+      this.chips.destroy()
+    }
   }
 }
 </script>
